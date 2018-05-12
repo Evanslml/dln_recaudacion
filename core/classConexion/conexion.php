cd985309cce8f266a82e2d59da6c63f37c509fdc
Ivan Cruz Peña <jivancplml@gmail.com>
2018-05-11 19:15:44 -0500

    PRUEBAS GIT

--- core/classConexion/conexion.php
+++ core/classConexion/conexion.php
@@ -5,15 +5,15 @@
   }
   #Constantes de conexión
   
-  define('DB_HOST','localhost');
-  define('DB_USER','root');
-  define('DB_PASS','root');
-  define('DB_NAME','02_dln_recaudacion');
+//  define('DB_HOST','localhost');
+//  define('DB_USER','root');
+//  define('DB_PASS','root');
+//  define('DB_NAME','02_dln_recaudacion');
 
- //define('DB_HOST','localhost');
- //define('DB_USER','root');
- //define('DB_PASS','');
- //define('DB_NAME','02_dln_recaudacion');
+ define('DB_HOST','localhost');
+ define('DB_USER','root');
+ define('DB_PASS','');
+ define('DB_NAME','02_dln_recaudacion');
  
   //creamos la conexion
   class Conexion extends mysqli {
